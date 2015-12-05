e7542f019b299f8b89002f5f52cfd19840aa6902
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-05 15:36:17 -0300

    Vista hereda desde 'master-layout'. Agrega tabla para mostrar los resultados y ademas clases bootstrap.

--- resources/views/ramos/index.blade.php
+++ resources/views/ramos/index.blade.php
@@ -1,6 +1,26 @@
+@extends('master-layout')
 
-@foreach($ramos as $ramo)
-    {!! $ramo->nombre !!}
-    {!! $ramo->codigo !!}
-    {!! $ramo->creditos !!}
-@endforeach
+@section('contenido')
+
+    <table class="table table-hover">
+        <th>Codigo</th>
+        <th>Nombre</th>
+        <th>Creditos</th>
+        <th>Acciones</th>
+
+        @foreach($ramos as $ramo)
+            <tr>
+                <td>{!! $ramo->codigo !!} </td>
+                <td>{!! $ramo->nombre !!} </td>
+                <td> {!! $ramo->creditos !!}</td>
+                <td>
+                    <a class="btn btn-default btn-xs btn-success" href="{!! URL::to('/ramos/'.$ramo->id) !!}" role="button">Ver</a>
+                    <a class="btn btn-default btn-xs btn-warning" href="{!! URL::to('/ramos/'.$ramo->id.'/edit') !!}" role="button">Editar</a>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+
+    {!! $ramos->render() !!}
+
+@endsection
