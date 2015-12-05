8e6c560f0c5c131f22ccc905e4810a0fdf263491
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-05 16:09:15 -0300

    Agrega rutas para gestionar ramos

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('ramos', 'RamoController@index'); // Mostrar todos los ramos
+Route::get('ramos/create', 'RamoController@create'); // Mostrar el formulario para crear un nuevo ramo
+Route::get('ramos/{id}', 'RamoController@show'); // Mostrar un ramo en particular, identificado con PK 'id'
+Route::post('ramos', 'RamoController@store'); // Almacenar los datos enviados desde el formulario de creacion
+Route::get('ramos/{id}/edit', 'RamoController@edit'); // Muestra un formulario pre-cargado con los datos del ramo 'id'
+Route::put('ramos/{id}', 'RamoController@update'); // Actualiza los datos desde el formulario para editar un ramo
 
-Route::get('ramos', 'RamoController@index');
-Route::get('ramos/{id}', 'RamoController@show');
