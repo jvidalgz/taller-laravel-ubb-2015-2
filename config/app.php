7dd35a477c2225e21dcc4b2edc21ca2ef664fc55
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-05 16:04:17 -0300

    Agrega dependencias para el proyecto: 1) Herramienta de Debug (http://raveren.github.io/kint/) 2) LaravelCollective (https://laravelcollective.com/) 3) Laravel-debugbar (https://github.com/barryvdh/laravel-debugbar)

--- config/app.php
+++ config/app.php
@@ -136,6 +136,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class, // provider para laravel debugbar
 
         /*
          * Application Service Providers...
@@ -193,6 +195,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
