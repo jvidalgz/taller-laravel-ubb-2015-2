a427c0752225e6aa3c98463fbd0bdbc82cf9c190
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-05 16:01:49 -0300

    Vista hereda desde 'master-layout'. Agrega 'breadcrum'

--- resources/views/ramos/show.blade.php
+++ resources/views/ramos/show.blade.php
@@ -1,3 +1,12 @@
-{!! $ramo->nombre !!}
-{!! $ramo->codigo !!}
-{!! $ramo->creditos !!}
+@extends('master-layout')
+
+    @section('contenido')
+        <ol class="breadcrumb">
+            <li><a href="{!! URL::to('ramos') !!}">Ramos</a></li>
+            <li>{!! $ramo->nombre !!}</li>
+        </ol>
+        <h1>Viendo ramo {!! $ramo->nombre !!}</h1>
+        {!! $ramo->nombre !!}
+        {!! $ramo->codigo !!}
+        {!! $ramo->creditos !!}
+    @endsection
